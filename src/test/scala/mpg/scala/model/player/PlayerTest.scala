dbfa67fe41c123fb61fd02a8376ada7b7908ff3f
Michi <michi-lang08@web.de>
2019-11-04 17:26:17 +0100

    Added tests for Player

--- src/test/scala/mpg/scala/model/player/PlayerTest.scala
+++ src/test/scala/mpg/scala/model/player/PlayerTest.scala
@@ -4,4 +4,38 @@ import org.scalatest.{WordSpec}
 
 class PlayerTest extends WordSpec {
 
+
+
+
+  "A player" should "receive points if both clicked cards matches" in {
+
+    val player = new Player()
+    val isAMatch = player.compareClickedCards(cardOne, cardTwo)
+    assert(isAMatch, true)
+
+  }
+
+  "A players points" should "be calculated correct" in {
+
+    val player = new Player()
+    player.updatePoints(hasMatched) //true false
+    val actualPlayerPoints = player.getPoints
+    assert(actualPlayerPoints, 1)
+
+    player.updatePoints(hasMatched) //true false
+    actualPlayerPoints = player.getPoints
+    assert(actualPlayerPoints, 2)
+
+    player.updatePoints(hasNotMatched) //true false
+    actualPlayerPoints = player.getPoints
+    assert(actualPlayerPoints, 2)
+
+  }
+  "A players points" should{
+    "be start at zero" in {
+      val player = new Player()
+      val playerPoints = player.getPoints
+      assert(playerPoints, 0)
+  }}
+
 }
