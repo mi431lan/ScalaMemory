5b4a541484dbebe41353de14619b343d1533e775
Michi <michi-lang08@web.de>
2019-11-04 17:25:26 +0100

    Added tests for MemoryCard

--- src/test/scala/mpg/scala/model/memorycard/MemoryCardTest.scala
+++ src/test/scala/mpg/scala/model/memorycard/MemoryCardTest.scala
@@ -3,5 +3,20 @@ package mpg.scala.model.memorycard
 import org.scalatest.WordSpec
 
 class MemoryCardTest extends WordSpec {
+  "A Card" should "not be flipped when the game starts" in {
 
+    val card = new MemoryCard;
+    val cardState = card.getCardState(); // muss funktional nicht oop sein
+    assert(cardState, false)
+
+  }
+
+  "A Card" should "be flipped on clickAction" in {
+
+    val card = new MemoryCard();
+    card.onClickAction();
+    val cardState = card.getCardState();
+    assert(cardState, true)
+
+  }
 }
