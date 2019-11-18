87802f487b91767c9b26e0c1d35a16e065b6497d
Giana <giana95@gmx.de>
2019-11-18 13:34:07 +0100

    modified TestMethods

--- src/test/scala/mpg/scala/model/player/PlayerTest.scala
+++ src/test/scala/mpg/scala/model/player/PlayerTest.scala
@@ -1,43 +1,54 @@
-/*
+
 package mpg.scala.model.player
 
-import org.scalatest.{WordSpec}
+import mpg.scala.model.memorycard.MemoryCard
+import org.scalatest.WordSpec
 
 class PlayerTest extends WordSpec {
 
 
-
-   "A player" should "receive points if both clicked cards matches" in {
-
-    val player = new Player()
-    val isAMatch = player.compareClickedCards(cardOne, cardTwo)
-    assert(isAMatch, true)
-
-  }
-
-  "A players points" should "be calculated correct" in {
-
-    val player = new Player()
-    player.updatePoints(hasMatched) //true false
-    val actualPlayerPoints = player.getPoints
-    assert(actualPlayerPoints, 1)
-
-    player.updatePoints(hasMatched) //true false
-    actualPlayerPoints = player.getPoints
-    assert(actualPlayerPoints, 2)
-
-    player.updatePoints(hasNotMatched) //true false
-    actualPlayerPoints = player.getPoints
-    assert(actualPlayerPoints, 2)
-
-  }
-  "A players points" should{
-    "be start at zero" in {
+  //Läuft durch
+  "A player" should {
+    "receive points if both clicked cards matches" in {
+      val cardOne = new MemoryCard
+      val cardTwo = new MemoryCard
       val player = new Player()
-      val playerPoints = player.getPoints
-      assert(playerPoints, 0)
-  }}
+      val isAMatch = player.compareClickedCards(cardOne, cardTwo)
+      assert(isAMatch, true)
+
+    }
+  }
+
+  def assertResult_(i: Int, value: 10): Any = 0
+
+  "A players points" should {
+    "be calculated correct" in {
+      val player = new Player()
+      player.updatePoints() //true false
+      val actualPlayerPoints: Int = player.getPoints()
+      assertResult_(actualPlayerPoints, 10) //das nehmen da Rückgabewert Int sein soll?
+
+
+      /* player.updatePoints() //true false
+      val actualPlayerPoints:Int = player.getPoints()
+        assertResult(player.getPoints(),0)
+
+        player.updatePoints() //true false
+        //val actualPlayerPoints:Int = player.getPoints()
+        assertResult(player.getPoints())*/
+
+    }
+  }
+//TODO: Nochmals prüfen
+    "A players points" should{
+      "be start at zero" in {
+        val player = new Player()
+        val playerPoints:Int = 0
+
+        assertResult(playerPoints, 0)
+    }}
+
 
 
 }
-*/
+
