36d24753769dc703b5b15cd9b0c8623ca0a54538
Michael Lang <mi431lan@htwg-konstanz.de>
2019-12-13 17:39:33 +0100

    Added Class Tui

--- src/main/scala/mpg/scala/ui/tui/Tui.scala
+++ src/main/scala/mpg/scala/ui/tui/Tui.scala
@@ -2,51 +2,69 @@ package mpg.scala.ui.tui
 
 import java.awt.Color
 
+import javax.swing.JMenuItem
 import mpg.scala.controller.board.BoardController
+import mpg.scala.controller.memorycard.MemoryCardController
 import mpg.scala.model.board.Board
 import mpg.scala.model.memorycard.MemoryCard
-import mpg.scala.ui.panels.BoardPanel
+import mpg.scala.model.player.Player
+import mpg.scala.observerpattern.{Observer, Subject}
+import mpg.scala.ui.panels.{BoardPanel, MemoryCardPanel}
 
+import scala.io.StdIn
 import scala.swing.{Dimension, Label, MainFrame}
 
-class Tui extends MainFrame {
-  title = "Scala Memory"
-  preferredSize = new Dimension(400, 400)
-
-  val label: Label = new Label {
-    text = "The button hasn't been clicked yet"
-  }
-
-  //TODO builder für karten set
-  //TODO zufallsgenerator für kartenposition
-  val vector: Vector[MemoryCard] = Vector(MemoryCard(Color.BLACK, Color.YELLOW, state = false),
+class Tui extends Observer {
+  var cards: Vector[MemoryCard] = Vector(MemoryCard(Color.BLACK, Color.YELLOW, state = false),
     MemoryCard(Color.BLACK, Color.YELLOW, state = false),
     MemoryCard(Color.BLACK, Color.BLUE, state = false),
     MemoryCard(Color.BLACK, Color.BLUE, state = false))
 
-  val board = new Board(vector,0)
-  val boardController = new BoardController(board)
-  title = "Grid Panel"
-  contents = new BoardPanel(0, 2, boardController)
-  /*new GridPanel(0, 2) {
-    contents += card1
+  val board = new Board(cards, 0)
+  val playerOne: Player = Player(isActive = true, 0)
+  val playerTwo: Player = Player(isActive = false, 0)
 
-    contents += Button("Close") {
-      sys.exit(0)
+  val boardController = new BoardController(board, playerOne, playerTwo)
+  boardController.addObserver(this)
+
+  def startGame(): Unit = {
+    println("###########################################")
+    println(":::::::::::::: WELCOME TO MEMORY ::::::::::")
+    println("###########################################")
+    println("\n")
+
+    println("Choose Card 1:")
+    chooseCard
+    println("Choose Card 1:")
+    chooseCard
+
+
+    if (boardController.compareCards()) {
+      println("Match!!!")
+    } else {
+      println("No Match!!!")
+
     }
-  }*/
-}
 
-object ScalaMemory {
-  def main(args: Array[String]) {
-    val ui = new Tui
-    ui.visible = true
-    println("End of main function")
+  }
+
+  def chooseCard: MemoryCard = {
+    val cardNr = StdIn.readInt()
+    boardController.addCard(cards.apply(cardNr - 1))
+  }
+
+  override def receiveCardUpdate(): Unit = {
+  }
+
+  override def receiveGameUpdate(boolean: Boolean): Unit = {
+
   }
 }
 
-//http://otfried.org/scala/tictactoe.html
-// tui und ui bauen muss beides event basiert sein
-// statt arrays vektoren verwenden da immutable
-// datenstruktur sollte immutable sein
-// klassen mit konstruktor, statt state zu verändern kann neue kopie erstellt werden, änderungen können über den konstruktor an die kopie übergeben werden
+object Main {
+  def main(args: Array[String]): Unit = {
+
+    val tui = new Tui
+    tui.startGame()
+  }
+}
