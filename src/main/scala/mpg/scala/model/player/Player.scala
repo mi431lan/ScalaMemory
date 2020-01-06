531230c584aed0817794c6744384ac398e099023
Michael Lang <mi431lan@htwg-konstanz.de>
2020-01-06 10:54:09 +0100

    Removed obsolete parameters

--- src/main/scala/mpg/scala/model/player/Player.scala
+++ src/main/scala/mpg/scala/model/player/Player.scala
@@ -6,19 +6,17 @@ import mpg.scala.model.memorycard.MemoryCard
 case class Player(isActive: Boolean, points: Int) {
 
 
-  def addPoint(point: Int): Player = {
-    Player(isActive, points + 1)
+  def addPoint(): Player = {
 
+    Player(isActive, points + 1)
   }
 
   def changeActive(): Player = {
+
     if (isActive) {
       Player(isActive = false, points)
     } else {
       Player(isActive = true, points)
-
     }
-
   }
-
 }
