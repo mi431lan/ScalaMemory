c83ef8f9112f04dba4368c17992cb12585b93e53
Michael Lang <mi431lan@htwg-konstanz.de>
2020-01-06 10:18:28 +0100

    Added random card creation, clean up

--- src/main/scala/mpg/scala/ui/gui/Gui.scala
+++ src/main/scala/mpg/scala/ui/gui/Gui.scala
@@ -4,44 +4,63 @@ import java.awt.Color
 
 import javax.swing.JMenuItem
 import mpg.scala.controller.board.BoardController
-import mpg.scala.model.board.Board
+import mpg.scala.model.board.{Board, CardCreator}
 import mpg.scala.model.memorycard.MemoryCard
 import mpg.scala.model.player.Player
 import mpg.scala.ui.panels.BoardPanel
 
-import scala.swing.{Dimension, Label, MainFrame}
+import scala.swing.event.ButtonClicked
+import scala.swing.{Button, Dimension, GridPanel, Label, MainFrame}
 
 class Gui extends MainFrame {
   title = "Scala Memory"
-  preferredSize = new Dimension(900, 900)
-
-  val label: Label = new Label {
-    text = "The button hasn't been clicked yet"
+  var start: Label = new Label {
+    text = "Choose difficulty!"
   }
-  //TODO builder für karten set
-  //TODO zufallsgenerator für kartenposition
-  val vector: Vector[MemoryCard] = Vector(MemoryCard(Color.BLACK, Color.YELLOW, state = false),
-    MemoryCard(Color.BLACK, Color.YELLOW, state = false),
-    MemoryCard(Color.BLACK, Color.BLUE, state = false),
-    MemoryCard(Color.BLACK, Color.BLUE, state = false),MemoryCard(Color.BLACK, Color.YELLOW, state = false),
-    MemoryCard(Color.BLACK, Color.YELLOW, state = false),
-    MemoryCard(Color.BLACK, Color.BLUE, state = false),
-    MemoryCard(Color.BLACK, Color.BLUE, state = false))
+  val buttonEasy: Button = new Button {
+    text = "Easy (12 cards)"
+    enabled = true
+  }
+  val buttonMedium: Button = new Button {
+    text = "Medium (24 cards)"
+    enabled = true
+  }
+  val buttonHard: Button = new Button {
+    text = "Hard (36 cards)"
+    enabled = true
+  }
+  val gridPanel: GridPanel = new GridPanel(0, 1) {
+    contents += start
+    contents += buttonEasy
+    contents += buttonMedium
+    contents += buttonHard
 
-  val board = new Board(vector,0)
-  val playerOne: Player = Player(isActive = true, 0)
-  val playerTwo: Player = Player(isActive = false, 0)
-  val boardController = new BoardController(board,playerOne,playerTwo)
-  title = "Grid Panel"
-  contents = new BoardPanel(0, 2, boardController)
+  }
+  contents = gridPanel
 
-  /*new GridPanel(0, 2) {
-    contents += card1
+  listenTo(buttonEasy, buttonMedium, buttonHard)
+  var board: Board = _
+  reactions += {
+    case ButtonClicked(component) if component == buttonEasy =>
+      board = Board(CardCreator().createRandomCardSet(12))
+      contents = createGame(12)
+    case ButtonClicked(component) if component == buttonMedium =>
+      board = Board(CardCreator().createRandomCardSet(24))
+      contents = createGame(24)
+    case ButtonClicked(component) if component == buttonHard =>
+      board = Board(CardCreator().createRandomCardSet(36))
+      contents = createGame(36)
 
-    contents += Button("Close") {
-      sys.exit(0)
-    }
-  }*/
+
+  }
+
+  def createGame(cards:Int): BoardPanel = {
+    val playerOne: Player = Player(isActive = true, 0)
+    val playerTwo: Player = Player(isActive = false, 0)
+    val boardController = new BoardController(board, playerOne, playerTwo)
+    new BoardPanel(cards/4, 6, boardController)
+  }
+
 }
 
 object ScalaMemory {
@@ -52,8 +71,3 @@ object ScalaMemory {
   }
 }
 
-//http://otfried.org/scala/tictactoe.html
-// tui und ui bauen muss beides event basiert sein
-// statt arrays vektoren verwenden da immutable
-// datenstruktur sollte immutable sein
-// klassen mit konstruktor, statt state zu verändern kann neue kopie erstellt werden, änderungen können über den konstruktor an die kopie übergeben werden
