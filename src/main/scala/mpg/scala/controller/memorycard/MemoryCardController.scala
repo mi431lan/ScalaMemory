10c0c4c68317d6e1d7447d94cf683d2f1632593a
Michael Lang <mi431lan@htwg-konstanz.de>
2020-01-06 10:55:11 +0100

    Formatting, removed unused imports

--- src/main/scala/mpg/scala/controller/memorycard/MemoryCardController.scala
+++ src/main/scala/mpg/scala/controller/memorycard/MemoryCardController.scala
@@ -1,20 +1,18 @@
 package mpg.scala.controller.memorycard
 
 import java.awt.Color
-import java.awt.event.ActionListener
 
 import mpg.scala.model.memorycard.MemoryCard
-import mpg.scala.observerpattern.{Observer, Subject}
-import mpg.scala.ui.gui.Gui
-import mpg.scala.ui.panels.{BoardPanel, MemoryCardPanel}
-import mpg.scala.ui.tui.Tui
+import mpg.scala.observerpattern.{Subject}
 
 case class MemoryCardController(memoryCardModel: MemoryCard) extends Subject {
 
- var memoryCard:MemoryCard=memoryCardModel
-  def getCard:MemoryCard={
+  var memoryCard: MemoryCard = memoryCardModel
+
+  def getCard: MemoryCard = {
     memoryCard
   }
+
   def getBackSide: Color = {
 
     memoryCard.backSide
